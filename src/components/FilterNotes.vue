4f66e124da0a94bf26310491558a63698b692245
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2020-07-09 23:56:26 +0300

    fixed options

--- src/components/FilterNotes.vue
+++ src/components/FilterNotes.vue
@@ -2,13 +2,21 @@
     <div>
         Filter Results:
         <select @change="filterNotes($event)">
-        	<option value="5">5</option>
-        	<option value="10">10</option>
-        	<option value="25">25</option>
-        	<option value="50">50</option>
-            <option value="100">100</option>
             <option value="200">200</option>
+            <option value="100">100</option>
+            <option value="50">50</option>
+            <option value="25">25</option>
+            <option value="10">10</option>
+            <option value="5">5</option>
         </select>
+        <div class="legend">
+            <span>
+                <span class="completed-box"></span> Done
+            </span>
+            <span>
+                <span class="incompleted-box"></span> In Progress
+            </span>
+        </div>
     </div>
 </template>
 
@@ -26,5 +34,29 @@
         padding: 5px;
         margin-top: 20px;
         border: 1px solid #41B883;
+    }
+
+    .legend {
+        display: flex;
+        justify-content: left;
+        margin: 1rem 0 2rem 0;
+    }
+
+    .legend > span {
+        margin-right: 15px;
+    }
+
+    .incompleted-box {
+        width: 10px;
+        height: 10px;
+        background: #41B883;
+        display: inline-block;
+    }
+
+    .completed-box {
+        width: 10px;
+        height: 10px;
+        background: #35495E;
+        display: inline-block;
     }
 </style>
