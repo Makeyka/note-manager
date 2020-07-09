06d1d56a5bacbb3c44d7b47d9c83604c3205d34f
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2020-07-09 18:44:14 +0300

    updated option order

--- src/components/FilterNotes.vue
+++ src/components/FilterNotes.vue
@@ -2,12 +2,12 @@
     <div>
         Filter:
         <select @change="filterNotes($event)">
-            <option value="200">200</option>
+        	<option value="5">5</option>
+        	<option value="10">10</option>
+        	<option value="25">25</option>
+        	<option value="50">50</option>
             <option value="100">100</option>
-            <option value="50">50</option>
-            <option value="25">25</option>
-            <option value="10">10</option>
-            <option value="5">5</option>
+            <option value="200">200</option>
         </select>
     </div>
 </template>
