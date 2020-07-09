e21d9b0360e774bbb5e0bdc6f6eda3b0f3341e50
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2020-07-09 23:57:06 +0300

    added clear field function

--- src/components/AddNote.vue
+++ src/components/AddNote.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <h3>Add Note</h3>
+        <h1>Note Manager</h1>
         <div class="add">
             <form @submit="onSubmit">
-                <input type="text" v-model="title" placeholder="Add new note.." required>
+                <input id="add-note" type="text" v-model="title" placeholder="Type your note.." required>
                 <span style="width:5px;"></span>
-                <input type="submit" value="Submit">
+                <input @click="clear" type="submit" value="ADD NOTE">
             </form>
         </div>
     </div>
@@ -26,12 +26,21 @@
             onSubmit(event) {
                 event.preventDefault()
                 this.addNote(this.title)
+            },
+            clear() {
+                setTimeout(() => {
+                    document.querySelector('#add-note').value = ''
+                }, 250)
             }
         }
     }
 </script>
 
 <style scoped>
+    h1 {
+        color: #35495E;
+    }
+
     form {
         display: flex;
     }
@@ -40,6 +49,7 @@
         flex: 10;
         outline: 0;
         padding: 10px;
+        font-size: 20px;
         border: 1px solid #41B883;
         border-radius: 5px;
     }
@@ -48,8 +58,15 @@
         flex: 2;
         color: #FFF;
         cursor: pointer;
+        font-weight: bold;
         background: #41B883;
         border: 1px solid #41B883;
+        transition: background 250ms;
         border-radius: 5px;
+    }
+
+    input[type="submit"]:hover {
+        background: #35495E;
+        transition: background 250ms;
     }
 </style>
